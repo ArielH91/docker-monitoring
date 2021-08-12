18891e2ea3cb10834c69966f227530ac641a1367
Grzegorz Miacz <grzegorz@miacz.pl>
2021-08-12 15:22:53 +0200

    feat: working solution, please analyze.

--- src/Docker.java
+++ src/Docker.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Docker {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         try {
@@ -21,17 +21,24 @@ public class Docker {
             while ((line = processOutput.readLine()) != null) {
                 lineCounter++;
                 if (lineCounter == 1) continue;
+                while (line.contains("   ")) {
+                    line = line.replace("   ", "  ");
+                }
 
-               String line1 = line.trim().replaceAll("     ","  ");
-               String line2 = line1.trim().replaceAll("    ", "  ");
-                String[] containerData = line2.trim().replaceAll("   ","  ").split("  |\\n");
+                String[] containerData = line.split("  ");
                 DockerContainerData data = new DockerContainerData();
 
                 data.setContainerId(containerData[0]);
-                data.setContainerName(containerData[6]);
-                data.setContainerStatus(containerData[4]);
                 data.setContainerImage(containerData[1]);
-                data.setContainerPorts(containerData[5]);
+                data.setContainerStatus(containerData[4]);
+                if(containerData.length > 6) {
+                    data.setContainerPorts(containerData[5]);
+                    data.setContainerName(containerData[6]);
+                } else {
+                    data.setContainerPorts("None");
+                    data.setContainerName(containerData[5]);
+                }
+
 
                 System.out.println(data.toString());
             }
