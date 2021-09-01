a7e804d1a9840a7e9f5e0de7a03b015b3ead168c
Ariello <drowlbn@gmail.com>
2021-09-01 11:44:51 +0200

    Initial commit

--- src/DockerMonitoring.java
+++ src/DockerMonitoring.java
@@ -1,32 +1,31 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DockerMonitoring {
 
 
     public static void main(String[] args) {
 
-
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.execute(() -> {
 
-            DockerContainersMonitoringService containersMonitoringService = new DockerContainersMonitoringService();
-            while (true) {
-                containersMonitoringService.run();
-
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        executor.execute(() -> {
+        Future<?> future = executor.submit(() -> {
 
             DockerMonitoringInterface monitoringInterface = new DockerMonitoringInterface();
             monitoringInterface.run();
         });
 
+        executor.execute(() -> {
+
+            DockerContainersMonitoringService containersMonitoringService = new DockerContainersMonitoringService();
+
+                while (!future.isDone()) {
+                    containersMonitoringService.run();
+                }
+
+        });
+
         executor.shutdown();
+
     }
 }
