b0ecaba509c56e7b07a8b71da55f4ba8a5f91f98
Ariello <drowlbn@gmail.com>
2021-09-07 21:14:10 +0200

    Initial commit

--- src/DockerContainerDetails.java
+++ src/DockerContainerDetails.java
@@ -9,12 +9,14 @@ public class DockerContainerDetails {
     private final String containerHostname;
     private final String containerPath;
 
-
-/*    public DockerContainerDetails(String containerNetwork, String containerVolumes, String containerPlatform, String containerHostname, String containerPath) {
-        this.containerNetwork = containerNetwork;
-        this.containerVolumes = containerVolumes;
-        this.containerPlatform = containerPlatform;
-        this.containerHostname = containerHostname;
-        this.containerPath = containerPath;
-    }*/
+    @Override
+    public String toString() {
+        return "DockerContainerDetails{" +
+                "containerNetwork='" + containerNetwork + '\'' +
+                ", containerVolumes='" + containerVolumes + '\'' +
+                ", containerPlatform='" + containerPlatform + '\'' +
+                ", containerHostname='" + containerHostname + '\'' +
+                ", containerPath='" + containerPath + '\'' +
+                '}';
+    }
 }
